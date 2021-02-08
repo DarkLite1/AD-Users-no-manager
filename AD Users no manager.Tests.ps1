2c2c268d9a01519b819a967ad6aa82ab7048ac73
DarkLite1 <BrechtGijbels@gmail.com>
2021-02-08 14:42:22 +0100

    added Pester version 5 support

---
+++ AD Users no manager.Tests.ps1
@@ -0,0 +1,126 @@
+﻿#Requires -Modules ActiveDirectory, Assert, Pester
+#Requires -Version 5.1
+
+BeforeAll {
+    $testScript = $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+
+    $OutParams = @{
+        FilePath = (New-Item "TestDrive:/Test.txt" -ItemType File).FullName
+        Encoding = 'utf8'
+    }
+    
+    $testParams = @{
+        ScriptName  = 'Test'
+        ImportFile  = $OutParams.FilePath
+        SQLDatabase = 'PowerShell TEST'
+        LogFolder   = (New-Item "TestDrive:/log" -ItemType Directory).FullName
+    }
+    
+    $MailAdminParams = {
+        ($To -eq $ScriptAdmin) -and ($Priority -eq 'High') -and ($Subject -eq 'FAILURE')
+    }
+    
+    $ADOUExisting = Get-ADOrganizationalUnit -Filter * | 
+    Select-Object -First 1 -ExpandProperty DistinguishedName
+
+    Mock Invoke-Sqlcmd2
+    Mock Send-MailHC
+    Mock Write-EventLog
+    Mock Get-ADuserWithoutOCSHC
+    Mock Get-ADDisplayNameHC
+    Mock Get-ADDisplayNameFromSID
+    Mock Get-ADCircularGroupsHC
+    Mock Get-ADTSProfileHC
+    Mock Test-ADOUExistsHC { $true }
+}
+
+Describe 'Prerequisites' {
+    Context 'ImportFile' {
+        It 'mandatory parameter' {
+            (Get-Command $testScript).Parameters['ImportFile'].Attributes.Mandatory | 
+            Should -BeTrue
+        } 
+        It 'file not existing' {
+            $testNewParams = $testParams.Clone()
+            $testNewParams.ImportFile = 'NonExisting'
+
+            .$testScript @testNewParams
+
+            Should -Invoke Send-MailHC -Exactly 1 -ParameterFilter {
+                (&$MailAdminParams) -and 
+                ($Message -like "*Cannot find path*NonExisting*because it does not exist*")
+            }
+        } 
+        It 'skip comments' {
+            Mock Get-ADUser
+            Mock Get-ADGroup
+            Mock Get-ADComputer
+
+            $fakeInputFile = @"
+MailTo: bob@contoso.com
+ADGroup: BEL ATT Leaver
+# BEL ATT Leaver
+
+OUs: OU=Users,OU=BEL,OU=EU,DC=grouphc,DC=net                                                                         
+OUs: OU=Users,OU=NLD,OU=EU,DC=grouphc,DC=net                                                                         
+
+#OUs: OU=Users,OU=CZE,OU=EU,DC=grouphc,DC=net  
+"@
+            $fakeInputFile | Out-File @OutParams
+
+            .$testScript @testParams
+
+            $Expected = @(
+                'MailTo: bob@contoso.com'
+                'ADGroup: BEL ATT Leaver'
+                'OUs: OU=Users,OU=BEL,OU=EU,DC=grouphc,DC=net'
+                'OUs: OU=Users,OU=NLD,OU=EU,DC=grouphc,DC=net'
+            )
+
+            Assert-Equivalent -Actual $File -Expected $Expected
+        } 
+        It 'OU missing' {
+            $fakeInputFile = @"
+MailTo: bob@contoso.com
+ADGroup: BEL ATT Leaver                                                                      
+"@
+            $fakeInputFile | Out-File @OutParams
+
+            .$testScript @testParams
+
+            Should -Invoke Send-MailHC -Exactly 1 -ParameterFilter {
+                (&$MailAdminParams) -and 
+                ($Message -like "*no organizational units found*")
+            }
+        } 
+        It 'OU incorrect' {
+            $fakeInputFile = @"
+MailTo: bob@contoso.com
+ADGroup: BEL ATT Leaver
+OU=Does,DC=Not,Exist=net
+"@
+            $fakeInputFile | Out-File @OutParams
+
+            .$testScript @testParams
+
+            Should -Invoke Send-MailHC -Exactly 1 -ParameterFilter {
+                (&$MailAdminParams) -and 
+                ($Message -like "The supplied distinguishedName must belong to one of the following*")
+            }
+        } 
+        It 'MailTo missing' {
+            @"
+ADGroup: BEL ATT Leaver
+OU=Does,DC=Not,Exist=net
+$ADOUExisting
+"@ | Out-File @OutParams
+
+            .$testScript @testParams
+
+            Should -Invoke Send-MailHC -Exactly 1 -ParameterFilter {
+                (&$MailAdminParams) -and ($Message -like "*MailTo*")
+            }
+        } 
+    }
+}
+    
---
+++ AD Users no manager.ps1
@@ -0,0 +1,203 @@
+﻿<#
+    .SYNOPSIS
+        Report about all the users that don't have a manager assigned to them 
+        in the AD.
+
+    .DESCRIPTION
+        Report about all the users that don't have a manager assigned to them 
+        in the AD. The report is an e-mail containing the total number of users 
+        per country that don't have a manager. In attachment is an Excel sheet 
+        containing these specific users.
+
+        When an AD group name is provided with the parameter 'ADGroup' in the 
+        'ImportFile' we check if users are member of one of these groups and 
+        add an extra boolean column to the Excel file for each group name.
+
+        All results are stored in an SQL database for use in the Excel sheet by
+        the PivotTable. This to generate a line graph with an overview of 
+        progress throughout time.
+
+    .PARAMETER ImportFile
+        Contains all needed parameters:
+        - MailTo
+        - OU Distinguished names
+        - AD Group
+
+    .PARAMETER LogFolder
+        Location for the log files.
+#>
+
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory)]
+    [String]$ScriptName = 'AD Users no manager',
+    [Parameter(Mandatory)]
+    [String]$ImportFile,
+    [String]$SQLServerInstance = 'GRPSDFRAN0049',
+    [String]$SQLDatabase = 'PowerShell',
+    [String]$SQLTableReportUsersNoManager = 'ReportUsersNoManager',
+    [String]$LogFolder = $env:POWERSHELL_LOG_FOLDER,
+    [String]$ScriptAdmin = $env:POWERSHELL_SCRIPT_ADMIN
+)
+
+Begin {
+    Try {
+        $Now = Get-ScriptRuntimeHC -Start
+        Import-EventLogParamsHC -Source $ScriptName
+        Write-EventLog @EventStartParams
+
+        #region Logging
+        $LogParams = @{
+            LogFolder    = New-FolderHC -Path $LogFolder -ChildPath "AD Reports\AD Users no manager\$ScriptName"
+            Name         = $ScriptName
+            Date         = 'ScriptStartTime'
+            NoFormatting = $true
+        }
+        $LogFile = New-LogFileNameHC @LogParams
+        #endregion
+
+        $SQLParams = @{
+            ServerInstance    = $SQLServerInstance
+            Database          = $SQLDatabase
+            QueryTimeout      = '1000'
+            ConnectionTimeout = '20'
+            ErrorAction       = 'Stop'
+        }
+
+        $ImportFileName = (Get-Item $ImportFile -EA Stop).BaseName
+        $File = Get-Content $ImportFile -EA Stop | Remove-CommentsHC
+
+        if (-not ($MailTo = $File | Get-ValueFromArrayHC MailTo -Delimiter ',')) {
+            throw "No 'MailTo' found in the input file."
+        }
+
+        if (-not ($OUs = $File | Get-ValueFromArrayHC -Exclude MailTo, ADGroup)) {
+            throw 'No organizational units found in the input file.'
+        }
+
+        $ADGroups = foreach ($A in ($File | Get-ValueFromArrayHC ADGroup -Delimiter ',')) {
+            [PSCustomObject]@{
+                Name    = $A
+                Members = Get-ADGroupMember $A -Recursive
+            }
+        }
+    }
+    Catch {
+        Write-Warning $_
+        Send-MailHC -To $ScriptAdmin -Subject 'FAILURE' -Priority 'High' -Message $_ -Header $ScriptName
+        Write-EventLog @EventErrorParams -Message "FAILURE:`n`n- $_"
+        Write-EventLog @EventEndParams; Exit 1
+    }
+}
+
+Process {
+    Try {
+        $UsersNoManager = Get-AdUserNoManagerHC -OU $OUs -EA Stop
+
+        $HtmlOus = $OUs | ConvertTo-OuNameHC -OU | Sort-Object | 
+        ConvertTo-HtmlListHC -Header 'Organizational units:'
+
+        Switch (($UsersNoManager | Measure-Object).Count) {
+            '0' {
+                $Intro = "<p>All users have a manager assigned.</p>"
+                $Subject = "All users have a manager assigned"
+                $Priority = 'Normal'
+            }
+            '1' {
+                $Intro = "<p>Only <b>1 user</b> has <b>no manager</b>:</p>"
+                $Subject = "1 user has no manager"
+                $Priority = 'High'
+            }
+            Default {
+                $Intro = "<p><b>$_ users</b> have <b>no manager</b>:</p>"
+                $Subject = "$_ users have no manager"
+                $Priority = 'High'
+            }
+        }
+
+        if ($UsersNoManager) {
+            if ($ADGroups) {
+                $UsersNoManager | ForEach-Object {
+                    $Sam = $_.'Logon name'
+
+                    $Properties = [Ordered]@{}
+                    $ADGroups | ForEach-Object {
+                        $Properties.($_.Name) = $_.Members.SamAccountName -contains $Sam
+                    }
+
+                    $_ | Add-Member -NotePropertyMembers $Properties
+                }
+            }
+
+            $Results = $UsersNoManager | Group-Object Country | Select-Object Count, Name
+
+            foreach ($R in $Results) {
+                Invoke-Sqlcmd2 @SQLParams -Query "
+                    INSERT INTO $SQLTableReportUsersNoManager
+                    (RunDate, ImportFile, Country, Total)
+                    VALUES ('$("{0:yyyy-MM-dd HH:mm:ss}" -f $Now)',
+                        '$ImportFileName', '$($R.Name)', '$($R.Count)')"
+            }
+
+            $Results = Invoke-Sqlcmd2 @SQLParams -As PSObject -Query "
+                SELECT * FROM $SQLTableReportUsersNoManager WHERE ImportFile = '$ImportFileName'"
+
+            $ExcelParams = @{
+                Path          = $LogFile + '.xlsx'
+                AutoSize      = $true
+                FreezeTopRow  = $true
+            }
+
+            $UsersNoManager | Export-Excel @ExcelParams -WorkSheetName Users -TableName User -NoNumberConversion 'Employee ID',
+                'OfficePhone', 'HomePhone', 'MobilePhone', 'ipPhone', 'Fax', 'Pager'
+
+            $Results | Export-Excel -Path $ExcelParams.Path -WorkSheetName HistoryLine -PivotRows RunDate -PivotColumns Country -PivotData @{Total = 'Sum'} -ChartType Line -IncludePivotTable -IncludePivotChart -HideSheet HistoryLine
+
+            $Results | Export-Excel -Path $ExcelParams.Path -WorkSheetName HistoryBar -PivotRows Country -PivotColumns RunDate -PivotData @{Total = 'Sum'} -ChartType ColumnClustered -IncludePivotChart -IncludePivotTable -HideSheet HistoryBar
+
+            $Table = $UsersNoManager | Group-Object Country |
+                Select-Object @{Name = "Country"; Expression = {$_."Name"}}, @{Name = "Total"; Expression = {$_."Count"}} |
+                Sort-Object Country | ConvertTo-Html -As Table -Fragment
+
+            $Message = "$Intro
+                        $Table
+                        <p><i>* Check the attachment for details and an historical overview</i></p>"
+        }
+        else {
+            $Message = $Intro
+        }
+    }
+    Catch {
+        Write-Warning $_
+        Send-MailHC -To $ScriptAdmin -Subject 'FAILURE' -Priority 'High' -Message $_ -Header $ScriptName
+        Write-EventLog @EventErrorParams -Message "FAILURE:`n`n- $_"
+        Write-EventLog @EventEndParams; Exit 1
+    }
+}
+
+End {
+    Try {
+        $EmailParams = @{
+            To          = $MailTo
+            Bcc         = $ScriptAdmin
+            Subject     = $Subject
+            Message     = $Message, $HtmlOus
+            Attachments = $ExcelParams.Path
+            Priority    = $Priority
+            LogFolder   = $LogParams.LogFolder
+            Header      = $ScriptName
+            Save        = $LogFile + ' - Mail.html'
+        }
+        Remove-EmptyParamsHC $EmailParams
+        Get-ScriptRuntimeHC -Stop
+        Send-MailHC @EmailParams
+    }
+    Catch {
+        Write-Warning $_
+        Send-MailHC -To $ScriptAdmin -Subject 'FAILURE' -Priority 'High' -Message $_ -Header $ScriptName
+        Write-EventLog @EventErrorParams -Message "FAILURE:`n`n- $_"; Exit 1
+    }
+    Finally {
+        Write-EventLog @EventEndParams
+    }
+}
